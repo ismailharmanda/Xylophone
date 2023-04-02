dd56da98e3738df380f4e49d85b4ff5304fdf842
İsmail Harmanda <iharmanda@hotmail.com>
2023-04-02 17:49:47 +0300

    change the opacity of button when pressed in

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -10,10 +10,14 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha=0.5
         playSound(fileName: sender.currentTitle!)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha=1
+        }
     }
     
-    func playSound(fileName:String) {
+    func playSound(fileName:String="C") {
         let url = Bundle.main.url(forResource: fileName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
